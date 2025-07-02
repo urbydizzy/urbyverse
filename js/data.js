f20166b9fd09469c8d9e39ed3b9b5e70d37af5ef
urbydizzy <61482073+urbydizzy@users.noreply.github.com>
2025-07-02 01:49:32 +0000

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,17 @@
 var data = {
 	images: [
-		
+		{
+			src: ["https://www.imgchest.com/p/qb4z8mord4j"],
+			thumbnail: "https://cdn.imgchest.com/files/y8xcnlqqlb4.png",
+			title: "Untitled Bunny Suit Gift Art",
+			artist: "DarknesS",
+			artist_url: "https://bsky.app/profile/did:plc:3xdyjimydh6q3b6cg5z26mu3",
+			art_url: "https://bsky.app/profile/exaltedbrand.bsky.social/post/3lhyl4tr73k2p",
+			desc: "fighting heroes by moonlight <br>winning buns by daylight <br>never running from a real fight <br>she is the one wishing me a happy birthday 🌻🎂",
+			date_str: "",
+			tags: [""],
+			hidden: false
+		}
 		{
 			src: [""],
 			thumbnail: "",
