6861aeb17ce3255aa577207179c01a78ff3a6e47
urbydizzy <61482073+urbydizzy@users.noreply.github.com>
2025-07-02 20:27:09 +0000

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,18 @@
 var data = {
 	images: [
 		{
+			src: [""],
+			thumbnail: "",
+			title: "",
+			artist: "",
+			artist_url: "",
+			art_url: "",
+			desc: "",
+			date_str: "",
+			tags: ["fireemblem", "bravelydefault", "thrasir", "tiz", "agnes", "edea", "ringabel", "basedonfic", "gift"],
+			hidden: true
+		},
+		{
 			src: ["https://cdn.imgchest.com/files/y8xcnkvn3l4.jpg"],
 			thumbnail: "https://cdn.imgchest.com/files/y8xcnlqqlb4.png",
 			title: "Untitled Bunny Suit Gift Art",
@@ -9,7 +21,19 @@ var data = {
 			art_url: "https://bsky.app/profile/exaltedbrand.bsky.social/post/3lhyl4tr73k2p",
 			desc: "fighting heroes by moonlight / winning buns by daylight / never running from a real fight / she is the one wishing me a happy birthday 🌻🎂",
 			date_str: "2/12/25",
-			tags: ["fireemblem"],
+			tags: ["fireemblem", "thrasir", "gift"],
+			hidden: false
+		},
+		{
+			src: ["https://cdn.imgchest.com/files/4z9cvwjbkb7.jpg"],
+			thumbnail: "https://cdn.imgchest.com/files/7w6c2v6ww3y.jpg",
+			title: "The Missing Flock",
+			artist: "adalheidis",
+			artist_url: "https://bsky.app/profile/adalheidis.bsky.social",
+			art_url: "https://urbydizzy.tumblr.com/post/85339455187/adalheidis-i-looked-into-sketches-i-did-and",
+			desc: "The others teased him about it, of course. That no matter how long or far they traveled, Tiz would always be a shepherd.",
+			date_str: "5/10/14",
+			tags: ["bravelydefault", "tiz", "agnes", "edea", "ringabel", "basedonfic", "gift"],
 			hidden: false
 		},
 		{
