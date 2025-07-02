11b346d44b157f60e0714df2cf958399d81a0a0a
urbydizzy <61482073+urbydizzy@users.noreply.github.com>
2025-07-02 18:46:43 +0000

    Update data.js

--- js/data.js
+++ js/data.js
@@ -12,7 +12,7 @@ var data = {
 			tags: ["fireemblem", "thrasir", "gift"],
 			hidden: false
 		},
-				{
+		{
 			src: ["https://cdn.imgchest.com/files/4z9cvwjbkb7.jpg"],
 			thumbnail: "https://cdn.imgchest.com/files/7w6c2v6ww3y.jpg",
 			title: "The Missing Flock",
@@ -20,7 +20,7 @@ var data = {
 			artist_url: "https://bsky.app/profile/adalheidis.bsky.social",
 			art_url: "https://urbydizzy.tumblr.com/post/85339455187/adalheidis-i-looked-into-sketches-i-did-and",
 			desc: "The others teased him about it, of course. That no matter how long or far they traveled, Tiz would always be a shepherd.",
-			date_str: "[find me]",
+			date_str: "5/10/14",
 			tags: ["bravelydefault", "tiz", "agnes", "edea", "ringabel", "basedonfic", "gift"],
 			hidden: false
 		},
